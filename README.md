8b56c146a6b55a273159c274dc33fb28147d7fd3
Alfredo Sánchez <alfred3ks@gmail.com>
2023-03-19 09:22:59 +0100

    Configuracion de prettier y eslint en el proyecto.

---
+++ README.md
@@ -0,0 +1,5 @@
+## Testimonios.
+
+### Tarjetas de testimonios, realizado con vite - React.
+
+## Alfredo Sánchez - @alfred3ks
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-
 const App = () => {
-  return (
-    <div>
-      <h1>Hola</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Hola</h1>
+		</div>
+	);
 };
 
 export default App;
 <div>
-  <h1>Hola</h1>
+	<h1>Hola</h1>
 </div>;
