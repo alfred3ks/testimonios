4e3fb85210fea2c9e8953dd5c02a84a3af201951
Alfredo Sánchez <alfred3ks@gmail.com>
2023-03-20 12:08:40 +0100

    Agrego archivos de resets, y estilos globales de la aplicación.

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const testimonios = [
+	{
+		nombre: 'Shawn Wang',
+		pais: 'Singapur',
+		imagen: 'shawn',
+		cargo: 'Ingeniero de Software',
+		empresa: 'Amazon',
+		testimonio:
+			'"Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en ArtemisCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. ArtenisCamp cambió mi vida."'
+	},
+	{
+		nombre: 'Sarah Chima',
+		pais: 'Nigeria',
+		imagen: 'sarah',
+		cargo: 'Ingeniera de Software',
+		empresa: 'ChatDesk',
+		testimonio:
+			'"ArtemisCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble."'
+	},
+	{
+		nombre: 'Emma Bostian',
+		pais: 'Suecia',
+		imagen: 'emma',
+		cargo: 'Ingeniera de Software',
+		empresa: 'Spotify',
+		testimonio:
+			'"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de ArtemisCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en ArtemisCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."'
+	}
+];
+
+export default testimonios;
